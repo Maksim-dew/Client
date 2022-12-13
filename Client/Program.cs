b372a2b562a2ca44b974ea3c2941b5abc613d667
Maksim-dew <maksim.medvedev0307@mail.ru>
2022-12-13 23:23:22 +0300

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 7000);
+                Console.WriteLine("Client connected");
+
+                NetworkStream stream = client.GetStream();
+
+                string request = "I have an question";
+                byte[] bytesWrite = Encoding.ASCII.GetBytes(request);
+                stream.Write(bytesWrite, 0, bytesWrite.Length);
+                stream.Flush();
+                Console.WriteLine("Client sent request: " + bytesWrite.Length);
+
+                byte[] bytesRead = new byte[256];
+                int length = stream.Read(bytesRead, 0, bytesRead.Length);
+                string answer = Encoding.ASCII.GetString(bytesRead, 0, length);
+                Console.WriteLine(answer);
+
+
+                client.Close();
+                Console.WriteLine("Client closed");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
